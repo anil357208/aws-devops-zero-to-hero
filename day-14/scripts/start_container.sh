f855483b3d435978d4adab6e1b8e0e5386f25ed9
devops979 <devops979@gmail.com>
2025-03-18 16:01:21 +0530

    Deleted the files which created in wrong path and modified the script

--- day-14/scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull anil357/simple-python-flask:latest
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 anil357/simple-python-flask:latest
