c78ea6725312fcada54715e733051541481acbfb
devops979 <devops979@gmail.com>
2025-03-18 17:16:05 +0530

    commented the script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the list of running container IDs
-container_ids=docker ps | awk -F "" '{print $1}'
-
+#container_ids=docker ps | awk -F "" '{print $1}'
+echo "hi"
 # Stop and remove all running containers
-docker rm -f $container_ids
+#docker rm -f $container_ids
