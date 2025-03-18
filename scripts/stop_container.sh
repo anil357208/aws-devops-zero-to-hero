13965e7460795c0944a710c5beaf6ae48f96f18e
devops979 <devops979@gmail.com>
2025-03-18 16:57:21 +0530

    The script was modifies for stopping the containers

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-set -e
 
+# Get the list of running container IDs
+container_ids=$(docker ps -q)
 
-# Stop the running container (if any)
-container_id=docker ps | awk -F " " '{print $1}'
-docker rm -f $container_id
+# Check if there are any running containers
+if [ -z "$container_ids" ]; then
+  echo "No running containers found."
+  exit 0
+fi
+
+# Stop and remove all running containers
+docker rm -f $container_ids
+
+# Verify that all containers have been removed
+if [ $? -eq 0 ]; then
+  echo "All containers have been removed successfully."
+else
+  echo "Failed to remove some containers."
+  exit 1
+fi
